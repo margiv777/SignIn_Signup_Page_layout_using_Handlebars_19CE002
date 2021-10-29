d0875099ab7377524ef6ce7e55d892f27d44bf3d
margiv777 <margiv.amin2001@gmail.com>
2021-10-29 16:46:47 +0530

    main1

---
+++ server.js
@@ -0,0 +1,51 @@
+const express=require("express");
+const PORT=3000;
+const app=express();
+const handlebars=require("express-handlebars");
+
+app.use(express.static("public"));
+app.use(express.urlencoded({extended:false}));
+
+app.set('view engine', 'hbs');
+
+app.engine("hbs",handlebars({
+    layoutsDir:`${__dirname}/views/layouts`,
+    extname:"hbs",
+    defaultLayout:"index",
+    partialsDir:`${__dirname}/views/partials`
+}))
+
+
+
+app.get("/",(req,res)=>{
+    res.render("main");
+});
+app.get("/signin",(req,res)=>{
+    res.render("signin");
+    
+});
+app.post("/signin",(req,res)=>{
+    res.end(`
+    <div>
+    <h1>${req.body.email}</h1>
+    <h1>${req.body.password}</h1>
+    </div>`)
+   
+});
+app.get("/signup",(req,res)=>{
+    res.render("signup");
+});
+app.post("/signup",(req,res)=>{
+    res.end(`
+    <div>
+    <h1>${req.body.email}</h1>
+    <h1>${req.body.name}</h1>
+    <h1>${req.body.password}</h1>
+    <h1>${req.body.confirmpassword}</h1>
+    </div>`)
+});
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is listening on port ${PORT}`);
+})
